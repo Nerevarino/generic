8ae4795c12bfc3e78c90ca45ea02d6969eccf94c
Evgeniy Dronov <dronov-ep@yandex.ru>
2018-08-29 11:09:20 +0300

    making testpage 29.08.2018

--- components/Hello_World/comp.php
+++ components/Hello_World/comp.php
@@ -2,8 +2,7 @@
 
 $comp_text = "Привет, мир!";
 
-$comp_template = file_get_contents('comp.html');
+$comp_template = file_get_contents($component_dir . 'comp.html');
 
 $component_html = str_replace('{text}', $comp_text, $comp_template);
-echo $component_html;
 
--- pages/testcomp/test.php
+++ pages/testcomp/test.php
@@ -1,17 +1,28 @@
 <?php
 
-$template = file_get_contents("test.html");
+const PROJECT_NAME = 'generic';
+const PROJECT_DIR = '/home/evgeniy/projects/web/' . PROJECT_NAME . '/';
+const COMPONENTS_DIR = PROJECT_DIR . 'components/';
+const PAGES_DIR = PROJECT_DIR . 'pages/';
+const TESTCOMP_DIR = PAGES_DIR . 'testcomp/';
+
+
+
+$template = file_get_contents(TESTCOMP_DIR . 'test.html');
 
 $component_name = "Hello_World";
+$component_dir = COMPONENTS_DIR . $component_name . "/";
 
-$component_html = require("../../components/{$component_name}/comp.php");
-$component_css = "<link rel=\"stylesheet\" href=\"../../components/{$component_name}/comp.css\"/>";
-$component_js = "<script type=\"text/javascript\" src=\"../../components/{$component_name}/comp.js\"></script>";
+require(COMPONENTS_DIR . $component_name . "/comp.php");
+// $component_css = COMPONENTS_DIR . $component_name . "/comp.css";
+$component_css = "<link rel=\"stylesheet\" href=\"/components/{$component_name}/comp.css\"/>";
+// $component_js = COMPONENTS_DIR . $component_name . "/comp.js";
+$component_js = "<script type=\"text/javascript\" src=\"/components/{$component_name}/comp.js\"></script>";
 
 
 
 $page = str_replace('{component_css}', $component_css, $template);
-$page = str_replace('{component}', $component_html, $page);
+$page = str_replace('{component_html}', $component_html, $page);
 $page = str_replace('{component_js}', $component_js, $page);
 
 
