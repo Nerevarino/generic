9415c0a61c7f150c8ab7e1ee2e0d0083b044df2f
Evgeniy Dronov <evgeniy@Notebook.notebook.my>
2018-09-03 07:45:59 +0300

    morning 3.09.2018

--- learn/page.php
+++ learn/page.php
@@ -1,14 +1,57 @@
 <?php
 
-function getRequestData()
+class Page
 {
-    if(isset($_POST['data'])) {
 
-    } elseif(isset($_GET['data'])) {
-        
-    } else {
-        
+    protected $template = <<<TEMPLATE
+<!DOCTYPE html>
+<html >
+  <head >
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {styles}
+    <title >Тестовая страница</title>
+  </head>
+  <body >
+    <div >Это тестовая страница</div>
+    <div >{greetings}</div>
+  </body>
+  {scripts}
+</html>
+TEMPLATE;
+    protected $input_data;
+    protected $output_data;
+    
+    public function __construct()
+    {
+        if(isset($_POST['data'])) {
+            $this->input_data = json_decode($_POST['data']);
+        } elseif(isset($_GET['data'])){
+            $this->input_data = json_decode($_GET['data']);
+        } else { $this->input_data = null; }
+
+        if($this->input_data != null) {
+            echo $this;
+        } else {
+            echo $this;
+        }
+    }
+
+    public function __toString()
+    {
+        $greetings = "Привет от PHP!";
+        $places = array
+        (
+            '{styles}', '{greetings}', '{scripts}'   
+        );
+        $substitutes = array
+        (
+            '<link rel="stylesheet" href="page.html"/>',
+            $greetings,
+            '<script type="text/javascript" src="page.js"></script>'
+        );
+        return str_replace($places, $substitutes, $this->template);
     }
 }
 
-$request_data = getRequestData();
+$page = new Page();
